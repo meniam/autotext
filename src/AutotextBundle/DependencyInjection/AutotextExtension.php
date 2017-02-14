df00f8d9082f39bd4d20a4c08f439e2700c759f5
Eugene Myazin <meniam@gmail.com>
2017-02-15 00:20:22 +0300

    Removed something wrong

--- src/AutotextBundle/DependencyInjection/AutotextExtension.php
+++ src/AutotextBundle/DependencyInjection/AutotextExtension.php
@@ -6,8 +6,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Reference;
 
 class AutotextExtension extends Extension
 {
@@ -19,19 +17,8 @@ class AutotextExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        $config = array();
-        foreach ($configs as $subConfig) {
-            $config = array_merge($config, $subConfig);
-        }
-
-        foreach ($config as $alias => $options) {
-            $optionDef = new Definition($container->getParameter('autotext.class'));
-            $optionDef->setShared(false);
-            $optionDef->addArgument(new Reference('service.autotext'));
-        }
-        die;
     }
+
     /**
      * @return string
      */
